13ba90c33df0a9b5ecd6e986d6b2839331b9efb4
softdrum <drewzerg@gmail.com>
2020-10-16 09:02:55 +0300

    refactored main.js file

--- src/main.js
+++ src/main.js
@@ -2,38 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import BaseCard from '@/components/cards/BaseCard';
-import {
-  BootstrapVue,
-  BIcon,
-  BIconSearch,
-  BIconArrowClockwise,
-  BIconMailbox,
-  BIconBriefcase,
-  BIconGeoAlt,
-  BIconStar,
-  BIconPeople,
-  BIconPencilSquare,
-  BIconExclamationTriangleFill,
-  BIconArrowLeft,
-} from 'bootstrap-vue'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 // Install BootstrapVue
 Vue.use(BootstrapVue)
-Vue.component('BIcon', BIcon)
-Vue.component('BIconSearch', BIconSearch)
-Vue.component('BIconArrowClockwise', BIconArrowClockwise)
-Vue.component('BIconMailbox', BIconMailbox)
-Vue.component('BIconBriefcase', BIconBriefcase)
-Vue.component('BIconGeoAlt', BIconGeoAlt)
-Vue.component('BIconStar', BIconStar)
-Vue.component('BIconPeople', BIconPeople)
-Vue.component('BIconPencilSquare', BIconPencilSquare)
-Vue.component('BIconExclamationTriangleFill', BIconExclamationTriangleFill)
-Vue.component('BIconArrowLeft', BIconArrowLeft)
-Vue.component('base-card', BaseCard);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
+Vue.component('base-card', BaseCard);
 new Vue({
   router,
   store,
