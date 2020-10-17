23c217b83f7bc587da88b233e9adda15e6fad272
softdrum <drewzerg@gmail.com>
2020-10-17 11:14:35 +0300

    refactor: changed props and validation added

--- src/components/cards/UserCardMini.vue
+++ src/components/cards/UserCardMini.vue
@@ -1,22 +1,18 @@
 <template>
   <router-link
-    :to="`/user/${data.login}`"
-    style="text-decoration: none; color: #333"
+    :to="`/user/${login}`"
+    class="user-link"
   >
     <base-card>
       <div class="d-flex flex-column align-items-center justify-content-center">
         <b-avatar
-          class="mb-2"
-          :src="data.avatarURL"
+          :src="avatarURL"
           alt="avatar"
           size="4rem"
+          class="mb-2"
         />
-        <span v-text="data.login"></span>
-        <small
-          v-text="data.id"
-          class="text-muted"
-        >
-        </small>
+        <span v-text="login"></span>
+        <small v-text="id" class="text-muted"></small>
       </div>
     </base-card>
   </router-link>
@@ -26,10 +22,27 @@
 export default {
   name: 'UserCard',
   props: {
-    data: {
-      type: Object,
+    login: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        return /[a-zA-Z0-9]{1,3}/.test(value) && value;
+      }
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+    avatarURL: {
+      type: String,
       required: true,
     }
   }
 }
 </script>
+<style scoped>
+.user-link {
+  text-decoration: none;
+  color: #333
+}
+</style>
