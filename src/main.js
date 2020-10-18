545f912ca679873e6594829da4008b366f75baac
softdrum <drewzerg@gmail.com>
2020-10-18 10:16:29 +0300

    add page title mixin

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router'
-import store from './store'
+import router from './router';
+import store from './store';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import BaseCard from '@/components/cards/BaseCard';
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import titleMixin from './mixins/title.mixin';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
+Vue.mixin(titleMixin);
 
 Vue.component('base-card', BaseCard);
 new Vue({
