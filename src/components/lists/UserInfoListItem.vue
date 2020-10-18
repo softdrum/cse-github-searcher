8e17959f59fa37a3220504a0da6b890d7771c49d
softdrum <drewzerg@gmail.com>
2020-10-18 12:52:05 +0300

    fix icon size when text breaks on second line

--- src/components/lists/UserInfoListItem.vue
+++ src/components/lists/UserInfoListItem.vue
@@ -1,6 +1,8 @@
 <template>
-  <li class="d-flex">
-    <b-icon :icon="icon" class="mr-1"></b-icon>
+  <li class="d-flex align-center">
+    <div class="mr-1">
+      <b-icon :icon="icon"></b-icon>
+    </div>
     <h6 v-text="text"></h6>
   </li> 
 </template>
